6f3a6137d1823b666fae732ef1266b91ffe0ea26
Shaunak Patel <shpatel@digitalharbor.com>
2019-09-16 21:40:28 +0530

    create basic project skeleton

---
+++ src/main/java/com/sg/SocgenApplication.java
@@ -0,0 +1,13 @@
+package com.sg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocgenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocgenApplication.class, args);
+	}
+
+}
